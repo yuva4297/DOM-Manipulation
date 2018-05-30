9673bf82cb005c49556796aeebbcecee51c0702d
Yuvarani Ramu <yuvaranir@virtusa.com>
2018-05-30 18:24:50 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -3,34 +3,36 @@ function identifyById() {
 }
 
 function identifyByClass() {
-
+    console.log(document.getElementsByClassName('class1'));
 }
 
 function identifyByTag() {
-
+    console.log(document.getElementsByTagName("p"));
 }
 
 function changeBackground() {
-
+    document.getElementById('block1').style.backgroundColor = "blue";
 }
 
 function increaseFont() {
+    document.getElementById('block2').style.fontSize = "xx-large";
 }
 
 function changeFontColor() {
+    document.getElementById('block3').style.color = "green";
 
 }
-
 function revertColor() {
+    document.getElementById('block3').style.color = "black";
 
 }
 
 function hide() {
-
+    document.getElementById('block4').style.display = "none";
 }
 
-function changeBackColorForAllBox() {
-
+function changeBackColor(color, id) {
+  document.getElementById(i)
 }
 
 
