849bc7c2ff3357b2dd3b04074d5d65361f948bdb
Yuvarani Ramu <yuvaranir@virtusa.com>
2018-06-05 09:24:18 +0530

    Complete dom manipulation

--- js/app.js
+++ js/app.js
@@ -32,17 +32,54 @@ function hide() {
 }
 
 function changeBackColor(color, id) {
-  document.getElementById(i)
+  document.getElementsByClassName(id)[0].style.backgroundColor = color;
+  document.getElementsByClassName(id)[1].style.backgroundColor = color;
+  document.getElementsByClassName(id)[2].style.backgroundColor = color;
+}
+
+function numbersOnly(e)
+{
+    var charCode = e.keyCode;
+    if(charCode < 48 || charCode > 57)
+    {
+        return false;
+    }
+    return true;
 }
 
 
 function addAdjacent() {
+    document.getElementById('para2').innerHTML+="<p>I got generated on the fly...</p>"
 }
 
 function removePara() {
-
+    document.getElementById('para3').parentElement.removeChild(document.getElementById('para4'));
 }
 
 function animatePara() {
-
+        myMove();
+}
+function myMove(){
+    document.getElementById("box5").style.position="relative";
+    document.getElementById("box5").style.animationName="example";
+    document.getElementById("box5").style.animationDuration="5s";
+}
+function convert_upper(){
+    document.getElementById("inputBox2").value=document.getElementById("inputBox1").value.toUpperCase();
+}
+function convert_camel(){
+    var s=document.getElementById("inputBox3").value;
+    var x=s.split(" ");
+    s=x[0];
+    for(var i=1;i<x.length;i++){
+        x[i]=x[i].charAt(0).toUpperCase()+x[i].substr(1);
+        s+=x[i];
+    }
+    document.getElementById("inputBox3").value=s;
+}
+function browser_name(){
+    console.log(document.getElementById("browsers").value);
+}
+function change_color(){
+    document.getElementById("inputBox5").style.backgroundColor="pink";
 }
